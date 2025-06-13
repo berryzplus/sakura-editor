c3fd95765478db59be171b65ef02b50374444755
berryzplus <berryzplus@gmail.com>
2025-06-13 21:52:40 +0900

    ResourceHolder追加
    Windows APIにありがちな「使わなくなったらXXXを呼ぶ」のリソースを単純に扱えるようテンプレート化する。

---
+++ src/main/cpp/cxx_util/ResourceHolder.hpp
@@ -0,0 +1,29 @@
+﻿/*!	@file */
+/*
+	Copyright (C) 2025, Sakura Editor Organization
+
+	SPDX-License-Identifier: Zlib
+ */
+
+#pragma once
+
+namespace cxx_util {
+
+/*!
+ * リソースハンドルを保持するスマートポインタ
+ */
+template<typename T, auto Deleter>
+struct ResourceHolder
+{
+	using holder_type = std::unique_ptr<std::remove_pointer_t<T>, decltype(Deleter)>;
+	holder_type holder;
+
+	/* implicit */ ResourceHolder(T t) noexcept
+		: holder(t, Deleter)
+	{
+	}
+
+	/* implicit */ operator T() const noexcept { return holder.get(); }
+};
+
+} // end of namespace cxx_util
