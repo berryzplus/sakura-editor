429a1fb000539b1404c8f8b246461f0dd4726215
berryzplus <berryzplus@gmail.com>
2026-04-29 11:36:39 +0900

    Revert "検証用に入れたログを削除する"
    This reverts commit 158e5efb07addef9b4bdd4a9c8ad9cc564624c57.

--- sakura_core/_main/CControlTray.cpp
+++ sakura_core/_main/CControlTray.cpp
@@ -444,7 +444,9 @@ LRESULT CControlTray::DispatchEvent(
 
 	case MYWM_UIPI_CHECK:
 		/* エディタ－トレイ間でのUI特権分離の確認メッセージ */	// 2007.06.07 ryoji
-		::SendMessageW(HWND(lParam), MYWM_UIPI_CHECK, 0L, LPARAM(hWnd));	// 返事を返す
+		std::wcout << L"[UIPI] >>>> MYWM_UIPI_CHECK: hwndEdit=" << (ULONG_PTR)lParam << std::endl;
+		::SendMessage( (HWND)lParam, MYWM_UIPI_CHECK,  (WPARAM)0, (LPARAM)0 );	// 返事を返す
+		std::wcout << L"[UIPI] <<<< MYWM_UIPI_CHECK: lParam=" << (ULONG_PTR)lParam << std::endl;
 		return lParam;
 
 	case MYWM_HTMLHELP:
