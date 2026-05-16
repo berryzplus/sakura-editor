73609b3b06cc449f0e5eb78479179fa118458241
berryzplus <berryzplus@gmail.com>
2026-05-16 11:37:11 +0900

    PyTest(ローカル)

--- tools/SonarQube/Run-Tests.ps1
+++ tools/SonarQube/Run-Tests.ps1
@@ -14,3 +14,45 @@ $useOpenCppCoverage = ($env:GITHUB_ACTIONS -eq 'true') -or (-not ($productId -ma
 
 # Invoke Tests1.
 . "$PSScriptRoot\Run-Test.ps1" "$HomePath\$Platform\$Configuration\tests1.exe" $useOpenCppCoverage $HomePath
+
+# install Python requirements.
+$startArgs = @(
+  "-m", "pip",
+  "install",
+  "-r", "requirements.txt"
+)
+
+# Invoke command with OpenCppCoverage.
+$p = Start-Process `
+  -FilePath "python.exe" `
+  -ArgumentList $startArgs `
+  -NoNewWindow `
+  -WorkingDirectory $HomePath `
+  -PassThru `
+  -Wait
+
+if ($p.ExitCode -ne 0) {
+  throw "Python requirements installation was Failed."
+}
+
+# Invoke PyTest.
+$startArgs = @(
+  "--ignore=externals",
+  "--cov",
+  "--cov-config=src/test/py/.coveragerc",
+  "--cov-report=xml:pytest-coverage.xml",
+  "--cov-fail-under=80"
+)
+
+# Invoke command with OpenCppCoverage.
+$p = Start-Process `
+  -FilePath "python.exe" `
+  -ArgumentList $startArgs `
+  -NoNewWindow `
+  -WorkingDirectory $HomePath `
+  -PassThru `
+  -Wait
+
+if ($p.ExitCode -ne 0) {
+  throw "PyTest was Failed."
+}
